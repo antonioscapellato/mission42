dbae77abed0a5ae7a683fbe51f057bbc6dab27df
antonioscapellato <antonio.scape99@gmail.com>
2025-05-24 16:39:04 +0200

    update css

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -8,7 +8,7 @@ const ChatHeader = () => {
     <div className="w-full bg-oapcity-50 backdrop-blur-md fixed right-0 top-0 z-40 flex items-center p-4">
       {/* Assuming mission42.png is in the public directory */}
       <Image src="/mission42.png" alt="Mission 42 Logo" className="h-8 w-8 mr-2" radius={"sm"} />
-      <h1 className="text-lg font-thin">Mission42</h1>
+      <h1 className="text-lg font-light text-default-500">Mission42</h1>
     </div>
   );
 };
